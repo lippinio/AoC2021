c50963b04b6e897ec69d56fd014bf877fe76cabc
David Lips <david.lips@swissonline.ch>
2021-12-01 19:22:02 +0100

    1

---
+++ Helpers/PuzzleUtils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class PuzzleUtils
+{
+    private readonly HttpClient _httpClient;
+    private static string _baseUrl = "https://adventofcode.com";
+    private static string? _sessionCookie = Environment.GetEnvironmentVariable("AOC_SESSIONCOOKIE");
+    private readonly Uri puzzleUri;
+
+    public PuzzleUtils(int year, int day, HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        this.puzzleUri = new Uri($@"{_baseUrl}/{year}/day/{day}");
+    }
+
+    public async Task<int[]> LoadIntArrayInput()
+    {
+        HttpResponseMessage response;
+        
+        using (var requestMessage = new HttpRequestMessage(HttpMethod.Get, string.Concat(puzzleUri,"/input")))
+        {
+            requestMessage.Headers.Add("Cookie",string.Concat("session=",_sessionCookie));
+            response = await _httpClient.SendAsync(requestMessage);
+        }
+
+        response.EnsureSuccessStatusCode();
+        
+        var responseBody = await response.Content.ReadAsStringAsync();
+        
+        return Array.ConvertAll(responseBody.Split('\n').Where(s => s != "").ToArray(), int.Parse);
+    }
+
+    public async Task<bool> PostResult(int level, int answer)
+    {
+        HttpResponseMessage response;
+        
+        var content = new FormUrlEncodedContent(new[]
+        {
+            new KeyValuePair<string, string>("level", level.ToString()),
+            new KeyValuePair<string, string>("answer", answer.ToString())
+        });
+        
+        using (var requestMessage = new HttpRequestMessage(HttpMethod.Post, string.Concat(puzzleUri,"/answer")))
+        {
+            requestMessage.Content = content;
+            requestMessage.Headers.Add("Cookie",string.Concat("session=",_sessionCookie));
+            response = await _httpClient.SendAsync(requestMessage);
+        }
+        
+        response.EnsureSuccessStatusCode();
+        
+        var responseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(responseBody);
+        return responseBody.Contains("That's the right answer!");
+    }
+}
---
+++ _1/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    public static class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        private static async Task Main()
+        {
+            //Level 1
+            var puzzleUtils = new PuzzleUtils(2021,1, client);
+            var input = await puzzleUtils.LoadIntArrayInput();
+
+            var result = 0;
+            for (var i = 0; i < input.Length -1; i++)
+            {
+                if (input[i] < input[i+1])
+                {
+                    result++;
+                } 
+            }
+            
+            Console.WriteLine(result);
+            await puzzleUtils.PostResult(level: 1, result);
+            
+            //Level 2
+            result = 0;
+            //for (var i = 0; i < input.Length -1; i++)
+            for (var i = input.Length - 3 - 1; i >= 0; i--)
+            {
+                //if (input[i] < input[i+1])
+                if (input[i] + input[i+1] + input[i+2] < input[i + 1]+ input[i+2] + input[i+3])
+                {
+                    result++;
+                } 
+            }
+
+            Console.WriteLine(result);
+            
+            await puzzleUtils.PostResult(2, result);
+        }
+    }
+}
